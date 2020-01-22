29d61d098002ffbbbcf7efeb18bbc6d63e23d8af
andrewjpoole <andrewjpoole@gmail.com>
2020-01-22 23:54:39 +0000

    Added setelasticsearchirl and setjsonserialisationoptions methods to the interface

--- src/IElasticBand.cs
+++ src/IElasticBand.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections.Generic;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace AJP.ElasticBand
 {
     public interface IElasticBand
     {
+        void SetElasticsearchUrl(string url);
+        void SetJsonSerialiserOptions(JsonSerializerOptions options);
+
         Task<ElasticBandResponse> Delete(string index, string id);
         Task<ElasticBandResponse<T>> GetDocumentByID<T>(string index, string id);
         Task<ElasticBandResponse<T>> Index<T>(string index, T data, string id = null);
