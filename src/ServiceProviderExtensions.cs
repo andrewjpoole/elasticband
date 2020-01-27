2e81530be2d0079d1081ebd01b9679c8722a3f19
andrewjpoole <andrewjpoole@gmail.com>
2020-01-27 22:22:50 +0000

    Added ServiceProvider extension method to add nessecary services into DI.

---
+++ src/ServiceProviderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AJP.ElasticBand
+{
+    public static class ServiceProviderExtensions
+    {
+        public static IServiceCollection AddElasticBand(this IServiceCollection services)
+        {
+            services
+                .AddHttpClient()
+                .AddSingleton<IElasticQueryBuilder, ElasticQueryBuilder>()
+                .AddSingleton<IElasticBand, ElasticBand>();
+
+            return services;
+        }
+    }
+}
